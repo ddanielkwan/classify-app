aabc9f6716a0a0a8e618e15cbaffc73df1a7fcd4
Daniel Kwan <danielkwan2000@gmail.com>
2021-01-02 14:14:14 -0500

    update utils clients

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+"""AWS get client utils"""
 import boto3
 def get_session():
     return boto3.Session(aws_access_key_id="xxx", aws_secret_access_key= "xxx")
@@ -6,4 +7,6 @@ def get_polly_client():
     session = get_session()
     return session.client('polly', 'ca-central-1')
 
-
+def get_transcribe_client():
+    session = get_session()
+    return session.client('transcribe', 'ca-central-1')
